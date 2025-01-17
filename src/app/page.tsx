d57bc5c403e849a89e426c5da2a5cf0785c90b60
joswinemmanuel <joswinemmanuel2002@gmail.com>
2025-01-18 01:18:22 +0530

    Changes 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
+                  I ve worked on a variety of projects, from simple
                   websites to complex web applications. Here are a few of my
                   favorites.
                 </p>
@@ -213,7 +213,7 @@ export default function Page() {
                 >
                   with a direct question on twitter
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
+                and I ll respond whenever I can. I will ignore all
                 soliciting.
               </p>
             </div>
